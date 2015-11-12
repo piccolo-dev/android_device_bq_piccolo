7645a380bab6cfb2b8612ee9275be6d0a133ab3f
Christian Morlok <ChristianMorlok@gmail.com>
2015-11-12 13:39:48 +0100

    display: Configure 4xx adreno device
    * Set opengl version to 3.1
    * Add Android Extension Pack (AEP) for 4xx adreno GPU
    
    More info: http://developer.android.com/guide/topics/graphics/opengl.html
    
    Change-Id: I98580eca8eb26f30eef78bdb8713dd5d09558166

--- device.mk
+++ device.mk
@@ -192,6 +192,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
