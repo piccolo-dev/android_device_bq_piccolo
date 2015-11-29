c3ee60823b9bca91def3a48fed1ddce920518c34
Kra1o5 <kra1o5x@gmail.com>
2015-11-29 22:32:00 +0100

    sepolicy: Set to permissive for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_KERNEL_SOURCE := kernel/bq/piccolo
 TARGET_KERNEL_CONFIG := cyanogenmod_piccolo_defconfig
 TARGET_KERNEL_ARCH := arm
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
