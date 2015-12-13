388a7347f2e7d494daf8266ff9dfee3093db8d8a
Kra1o5 <kra1o5x@gmail.com>
2015-12-13 10:37:51 +0100

    Update device & product name

--- cm.mk
+++ cm.mk
@@ -11,9 +11,9 @@ $(call inherit-product, device/bq/piccolo/full_piccolo.mk)
 PRODUCT_RELEASE_NAME := Aquaris M5
 PRODUCT_NAME := cm_piccolo
 
-# Set product name
+# Set product device & name
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=Aquaris_M5
+   TARGET_DEVICE=Aquaris_M5 PRODUCT_NAME=Aquaris_M5
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=bq/Aquaris_M5/Aquaris_M5:5.0.2/LRX22G/1445393882:user/release-keys \
