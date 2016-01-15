bceb57e214f4d787c926131f31a6385ee9330857
Kra1o5 <kra1o5x@gmail.com>
2016-01-15 17:23:53 +0100

    piccolo: Build GPS HAL
    * Based on TAG LA.BR.1.1.3-03710-8x16.0 from CAF.

--- device.mk
+++ device.mk
@@ -107,6 +107,9 @@ PRODUCT_PACKAGES += \
     qcom.fmradio
 
 # GPS
+PRODUCT_PACKAGES += \
+    gps.msm8916
+    
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
     $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
