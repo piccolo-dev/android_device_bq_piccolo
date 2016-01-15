39054191769e558c820c355fb2c1433cec1478d4
Kra1o5 <kra1o5x@gmail.com>
2016-01-15 17:37:16 +0100

    Revert "piccolo: Dont copy GPS files for now"
    This reverts commit 87e2ee78562f04aee3203ef1ae2c37a9380e48f4.

--- device.mk
+++ device.mk
@@ -114,12 +114,12 @@ PRODUCT_PACKAGES += \
     libfmjni
 
 # GPS
-#PRODUCT_COPY_FILES += \
-#    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
-#    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
-#    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
-#    $(LOCAL_PATH)/gps/etc/quipc.conf:system/etc/quipc.conf \
-#    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/quipc.conf:system/etc/quipc.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf
 
 # IO Scheduler
 PRODUCT_PROPERTY_OVERRIDES += \
